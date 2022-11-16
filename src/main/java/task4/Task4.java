564ea94984b635b9b4df257c25912d5c69dc9fe3
79100 <@>
2022-11-16 16:39:06 +0300

    1

--- src/main/java/task4/Task4.java
+++ src/main/java/task4/Task4.java
@@ -1,5 +1,6 @@
 package task4;
 
+import javax.print.DocFlavor;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -8,10 +9,11 @@ import java.util.Scanner;
 
 public class Task4 {
     public static void main(String[] args) throws FileNotFoundException {
-        File fileName = new File("C:/task4.txt");
+        Scanner input = new Scanner(System.in);
+        String file = input.nextLine();
 
         List<Double> listElement = new ArrayList<>();
-        Scanner scanner = new Scanner(fileName);
+        Scanner scanner = new Scanner(new File(file));
         while (scanner.hasNext()) {
             String line = scanner.nextLine();
             listElement.add(Double.parseDouble(line));
